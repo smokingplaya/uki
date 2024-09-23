040d7fd73004901cd0fcb5a674d58bd7b5e777e4
smokingplaya <smokingplaya@yandex.ru>
2024-09-23 13:18:14 +0300

    0.2 :: patch :: upd comments

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@
  * uki
  * created by smokingplaya 2024
  *
- * uki 0.2
  * changes:
- *  rewrited code and becomes clearly
- *  now we using serde_yml
- *  now yml-config file doesn't requires separate folder, it just uses .uki file in cwd
- *  yml-config file changed structure, now its more like to smokingplaya/runny file structure
- *  added command history; now you can works with cd/pwd etc.
+ * rewrote the code so it's clearer.
+ * now we use serde_yml
+ * now the yml-config file doesn't need a separate folder, it just uses the .uki file in cwd
+ * changed the structure of the yml-config file, now it looks more like the smokingplaya/runny file structure
+ * added command history; now you can work with cd/pwd etc.
  */
 
 pub(crate) mod configuration;
