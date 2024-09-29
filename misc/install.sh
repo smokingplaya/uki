23c30f17a362c0346f3f2ff82d9c43b5e6c33d55
smokingplaya <82664853+smokingplaya@users.noreply.github.com>
2024-09-29 21:09:54 +0300

    Create install.sh

---
+++ misc/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if ! command -v curl > /dev/null 2>&1; then
+  echo "Execution interruption: curl is not installed"
+  exit 1
+fi
+
+# you can customize installation process
+URL="https://github.com/smokingplaya/uki/releases/latest/download/uki-linux_x86"
+OUTPUT="uki"
+
+curl -o $OUTPUT $URL
+chmod +x $OUTPUT
+sudo mv $OUTPUT /usr/local/bin
+echo "The file was successfully moved to /usr/local/bin"
